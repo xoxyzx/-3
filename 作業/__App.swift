eb55dfe6273ce3cddfd52de141b38cb692eaa222
陳廷祥 <xoxyzx@gmail.com>
2022-11-21 21:03:11 +0800

    Initial Commit

---
+++ 作業/__App.swift
@@ -0,0 +1,17 @@
+//
+//  __App.swift
+//  作業
+//
+//  Created by 許雯淇 on 2022/11/21.
+//
+
+import SwiftUI
+
+@main
+struct __App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
